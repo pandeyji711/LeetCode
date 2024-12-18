76f959a9015f68cb574d27dc2f13e00355cec3cd
Anurag Pandey <anuragpandey88699@gmail.com>
2024-12-18 18:37:49 +0530

    easy

---
+++ 1475.final-prices-with-a-special-discount-in-a-shop/finalPrices.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        // vector<int>ans;
+        for(int i=0;i<prices.size();i++)
+        {
+              int dis=0;
+               for( int j=i+1;j<prices.size();j++)
+               {
+                          if(prices[j]<=prices[i])
+                          {
+                                dis=prices[j];
+                                break;
+                          }
+               }
+                prices[i]-=dis;
+        }
+        return prices;
+    }
+};
