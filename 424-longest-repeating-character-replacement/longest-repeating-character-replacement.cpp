19625e9df4c108d08ab554dede8bf63fcbd51cfd
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-04-05 17:28:14 +0530

    Time: 19 ms (13.46%) | Memory: 10.9 MB (43.17%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -13,51 +13,9 @@ public:
           if(sum-maxx<=k)return true;
           return false;
   }
-  bool solve(string &s, int &k,int mid)
-  {
-           unordered_map<char,int>m;
-           int cnt =mid;
-           int l=0;
-           int r=0;
-           while(cnt--)
-           {
-                 m[s[r]]++;
-                 r++;
-           }
-
-           if(possible(m,k))
-           return true;
-        //    r++;
-           while(r<s.length())
-           {
-                   m[s[r]]++;
-                   m[s[l]]--;
-                   if(m[s[l]]<=0)
-                   m.erase(s[l]);
-                    if(possible(m,k))
-                      return true;
-                      r++;
-                      l++;
-
-           }
-           return false;
-  }
-    int characterReplacement(string s, int k) {
+    int characterReplacement(string &s, int k) {
         
-        // int l=0;
-        // int r=s.length();
-        // int ans=0;
-        // while(l<=r)
-        // {
-        //       int mid=l+(r-l)/2;
-        //       if(solve(s,k,mid))
-        //       {
-        //         //   cout<<mid<<endl;
-        //               ans=mid;
-        //               l=mid+1;
-        //       }else
-        //       r=mid-1;
-        // }
+      
          unordered_map<char,int>m;
         //    int cnt =mid;
         int ans=0;
