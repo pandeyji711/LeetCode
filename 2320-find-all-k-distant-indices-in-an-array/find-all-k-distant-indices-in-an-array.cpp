8db8a6ba68a64abfc1064f236495e4a8de0af3d7
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-04-13 17:56:59 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (28.27%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
@@ -18,33 +18,58 @@ public:
           }
   }
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
-        vector<int>vis(nums.size(),0);
+    queue<pair<int,int>>q;
         for(int i=0;i<nums.size();i++)
         {
-               if(nums[i]==key)
-               {
-                       //left
-            int left=max(0,i-k);
-          for(int l=i;l>=left;l--)
-          {
+              if(nums[i]==key)
+                 q.push({i-k,i+k+1});
+        }
+   queue<int>q2;
+    vector<int>ans;
+     for(int  i=0;i<nums.size();i++)
+     {
+                while(!q.empty()&&q.front().first<=i)
+                { 
+                    q2.push(q.front().second);
+                    // cout<<i<<" ";
+                    q.pop();
+                }
+                while(!q2.empty()&&q2.front()<=i)
+                {
+                      q2.pop();
+                }
+                if(q2.size()>0)
+                ans.push_back(i);
+     }
 
-               vis[l]=1;
-          }
-          //right
-         int n=vis.size()-1;
-          int right=min(n,i+k);
-          for(int l=i+1;l<=right;l++)
-          {
-              vis[l]=1;
-          }
-               }
-        }
-        vector<int>ans;
-        for(int i=0;i<vis.size();i++)
-        {
-              if(vis[i]==1)
-              ans.push_back(i);
-        }
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //        if(nums[i]==key)
+        //        {
+        //                //left
+        //     int left=max(0,i-k);
+        //   for(int l=i;l>=left;l--)
+        //   {
+        //     //    if(vis[l]==1)break;
+        //        vis[l]=1;
+        //   }
+        //   //right
+        //      int n=vis.size()-1;
+        //   int right=min(n,i+k);
+        //   for(int l=i+1;l<=right;l++)
+        //   {
+        //     //   if(vis[l]==1)break;
+        //       vis[l]=1;
+        //   }
+        //        }
+        // }
+        // vector<int>ans;
+        // for(int i=0;i<vis.size();i++)
+        // {
+
+        //       if(vis[i]==1)
+        //       ans.push_back(i);
+        // }
         return ans;
     }
 };
