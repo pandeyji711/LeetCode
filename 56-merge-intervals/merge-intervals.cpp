7177482571986f0342aeef69db13d3a1bcfd3c6c
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-04-16 01:07:15 +0530

    Time: 8 ms (28.80%) | Memory: 23.7 MB (92.99%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,44 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-       
-        queue<pair<int,int>>pq;
-        vector<int>v;
-       sort(intervals.begin(),intervals.end());
- for(int i=0;i<intervals.size();i++)
-        {
-                pq.push({intervals[i][0],intervals[i][1]});
-                v.push_back(intervals[i][0]);
-                v.push_back(intervals[i][1]);
-
-        }
-        priority_queue<int,vector<int>,greater<int>>pq1;
-       int  st=-1;
-       sort(v.begin(),v.end());
-       vector<vector<int>>ans;
-       for(int i=0;i<v.size();i++)
-       {
-                  while(!pq.empty()&&pq.front().first<=v[i])
-                  {
-                          pq1.push(pq.front().second);
-                    
-                          if(st==-1)
-                             st=v[i];
-
-                          pq.pop();
-
-                  }
-                  while(!pq1.empty()&&pq1.top()<=v[i])
-                  {
-                       pq1.pop();
-                  }
-                  if(pq1.empty()&&st!=-1)
-                  {
-                      
-                       ans.push_back({st,v[i]});
-                       st=-1;
-                  }
-       }
+     sort(intervals.begin(),intervals.end());
+     vector<vector<int>>ans;
+     ans.push_back(intervals[0]);
+     for(int i=1;i<intervals.size();i++)
+     {
+                     if(intervals[i][0]<=ans.back()[1])
+                     {
+                        ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+                     }
+                     else{
+                        ans.push_back(intervals[i]);
+                     }
+     }
        return ans;
     }
 };
