5fce2b3d2181b7b74ae77fc958f27bebc0db1f8c
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-04-19 17:27:58 +0530

    Time: 919 ms (5.03%) | Memory: 60.4 MB (73.14%) - LeetSync

---
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        sort(nums.begin(),nums.end());
+        //less then lower
+        long long low=0;
+        for(int i=1;i<nums.size();i++)
+        {
+              int maxx=lower-1-nums[i];
+              auto it=upper_bound(nums.begin(),nums.begin()+i,maxx);
+            //   if(it==nums.begin()+i)continue;
+            if(maxx<nums[0])continue;
+             int elements=0;
+                   if(it==nums.begin()+i)
+                   {
+                     elements=it-nums.begin();
+                   }else
+                   elements=it-nums.begin();
+                //    cout<<elements<<" ";
+                 cout<<elements<<" "<<i<<" "<<endl;
+                    low+=elements;
+
+              
+              
+        }
+        //greater ten upper
+        long long upr =0;
+        for(int i=0;i<nums.size();i++)
+        {
+                int atlest=upper-nums[i];
+                auto it=upper_bound(nums.begin()+i+1,nums.end(),atlest);
+                if(it==nums.end())continue;
+                else{
+                        
+                        int ele=nums.size()-(it-nums.begin());
+                        // cout<<ele<<" "<<i<<" "<<it-nums.begin() <<endl;
+                       upr+=ele;
+                }
+        }
+        cout<<low<<" "<<upr;
+        long long  n=nums.size();
+        long long total=n*(n-1)/2-(low+upr);
+        return total; 
+    }
+};
