2df89cf268634e330104a3052f9f096adba5ba28
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-04-21 00:53:51 +0530

    Time: 10 ms (67.96%) | Memory: 13.1 MB (78.80%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -3,17 +3,18 @@ public:
     int partitionString(string s) {
         // int l=0;
         // int r=0;
-        unordered_map<char,int>m;
+        vector<int>m(26,0);
+        // unordered_map<char,int>m;
         // char uni=s[0];
   int ans=0;
         for(int i=0;i<s.length();i++)
         {
-             m[s[i]]++;
-                  if(m[s[i]]>1)
+             m[s[i]-'a']++;
+                  if(m[s[i]-'a']>1)
                   {
                        ans++;
-                       m.clear();
-                       m[s[i]]++;
+                      fill(m.begin(), m.end(), 0);
+                       m[s[i]-'a']++;
 
                   }
         }
