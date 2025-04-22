1b4d975df44c7acad5a8523cac654b0cfccfdbb2
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-04-23 02:32:27 +0530

    Time: 8 ms (77.97%) | Memory: 51.8 MB (87.43%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
-int  dfs(vector<vector<int>>&adj,vector<int>&vis,int root)
+bool dfs(vector<vector<int>>&adj,vector<int>&vis,int root)
  {
-                      
-                      if(vis[root]!=0)return vis[root];
                       vis[root]=1;
-                    //   if(dp[root]==true)return true;
                       for( int a1:adj[root])
                       {
-    
-                          if(dfs(adj,vis,a1)==1)
+                          if(vis[a1]==1)return true;
+                          if(vis[a1]==0&&dfs(adj,vis,a1))
                           {
-                              return 1;
+                              return true;
                           }
                           
                       }
-                 
+                  vis[root]=2;
                     //   st.push(root);
-                      return  vis[root]=2;
+                      return false;
  }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         
         vector<int>ans;
-    
-
-    // vector<bool>dp(graph.size(),false);
-     vector<int>vis(graph.size(),0);
+   vector<int>vis(graph.size(),0);
            for(int i=0;i<graph.size();i++)
            {
                
-                  if(dfs(graph,vis,i)==2)
-                  { 
-                    ans.push_back(i);
-                    }
+                  if(!dfs(graph,vis,i))ans.push_back(i);
            }
            return ans;
     }
