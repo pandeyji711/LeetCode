3b8b873280417b42789d985a1c7c412a5290d047
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-04-25 01:20:06 +0530

    Time: 79 ms (27.94%) | Memory: 25 MB (55.50%) - LeetSync

---
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -0,0 +1,85 @@
+class Solution {
+public:
+    int minCost(vector<vector<int>>& grid) {
+       int n=grid.size();
+       int m=grid[0].size();
+        int x=n-1;
+        int y=m-1;
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+        vector<vector<int>>cost(n,vector<int>(m,INT_MAX));
+        pq.push({0,x,y});
+        cost[x][y]=0;
+        while(!pq.empty())
+        {
+              int x1=pq.top()[1];
+              int y1=pq.top()[2];
+              pq.pop();
+              //up
+              if(x1-1>=0)
+              {
+                     int pre=cost[x1-1][y1];
+                       int curr=((grid[x1-1][y1]==3)?0:1)+cost[x1][y1];
+                       if(pre>curr)
+                       {
+                           cost[x1-1][y1]=curr;
+                           pq.push({curr,x1-1,y1});
+                       }
+              }
+              //left
+              if(y1-1>=0)
+              {
+                     int pre=cost[x1][y1-1];
+
+                       int curr=((grid[x1][y1-1]==1)?0:1)+cost[x1][y1];
+                       if(pre>curr)
+                       {
+                           cost[x1][y1-1]=curr;
+                           pq.push({curr,x1,y1-1});
+                       }
+              }
+              //right
+              if(y1+1<m)
+              {
+                     int pre=cost[x1][y1+1];
+                       int curr=((grid[x1][y1+1]==2)?0:1)+cost[x1][y1];
+                       if(pre>curr)
+                       {
+                           cost[x1][y1+1]=curr;
+                           pq.push({curr,x1,y1+1});
+                       }
+              }
+              //down
+              if(x1+1<n)
+              {
+                     int pre=cost[x1+1][y1];
+                       int curr=((grid[x1+1][y1]==4)?0:1)+cost[x1][y1];
+                       if(pre>curr)
+                       {
+                           cost[x1+1][y1]=curr;
+                           pq.push({curr,x1+1,y1});
+                       }
+              }
+            //    for(int i=0;i<n;i++)
+            //    {
+            //   for(int j=0;j<m;j++)
+            //   {
+            //      cout<<cost[i][j]<<" ";
+
+            //   }
+            //   cout<<endl;
+            //   }
+            //   cout<<"=================================================="<<endl;
+        }
+        // for(int i=0;i<n;i++)
+        // {
+        //       for(int j=0;j<m;j++)
+        //       {
+        //          cout<<cost[i][j]<<" ";
+
+        //       }
+        //       cout<<endl;
+        // }
+        return cost[0][0];
+
+    }
+};
