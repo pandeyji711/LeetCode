b5730cb0495b58d6a66bff9942465256af475f57
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-05-16 20:32:51 +0530

    Time: 114 ms (27.35%) | Memory: 71 MB (18.75%) - LeetSync

---
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+   bool diff(string str1,string str2)
+   {
+        if(str1.length()!=str2.length())return false;
+     int cnt=0;
+
+            for(int i=0;i<str1.length();i++)
+            {
+                    if(str1[i]!=str2[i])
+                    cnt++;
+                    if(cnt>1)return false;
+            }
+            return cnt==1;
+   }
+  int solve(vector<string>& words, vector<int>& groups,int ind,int indp,  vector<vector<int>>&dp)
+  {
+           if(ind>=words.size())return 0;
+             //pck
+             if(dp[ind][indp+1]!=-1)
+             return dp[ind][indp+1];
+             int x=0;
+             int y=0;
+             if(indp==-1||groups[ind]!=groups[indp]&&diff(words[ind],words[indp]))
+             {
+                  x=1+solve(words,groups,ind+1,ind,dp);
+             }
+             y=solve(words,groups,ind+1,indp,dp);
+             return  dp[ind][indp+1]= max(x,y);
+
+  }
+     
+    vector<string> getWordsInLongestSubsequence(vector<string>& words, vector<int>& groups) {
+      vector<vector<int>>dp(words.size()+1,vector<int>(words.size()+1,-1));
+      vector<int>dp1(words.size()+1,1);
+      vector<int>ind(words.size()+1,-1);
+      int ans=1;
+      int stind=0;
+        for(int i=1;i<words.size();i++)
+        {
+              for(int j=0;j<i;j++)
+              {
+                      if(groups[i]!=groups[j]&&diff(words[i],words[j]))
+                      {
+                           if(dp1[i]<dp1[j]+1)
+                           {
+                               ind[i]=j;
+                           }
+                           dp1[i]=max(dp1[j]+1,dp1[i]);
+                           
+                        //    cout<<i<<" "<<dp1[i]<<endl;
+                           if(ans<dp1[i])
+                           {
+                                stind=i;
+                           }
+                           ans=max(ans,dp1[i]);
+                      }
+              }
+        }
+          vector<string>res;
+          res.push_back(words[stind]);
+          int next=ind[stind];
+          while(next!=-1)
+          {
+                  res.push_back(words[next]);
+                  next=ind[next];
+
+
+          }
+        //   for(int i=stind;i>=)
+    //   int x=solve(words,groups,0,-1,dp);
+        reverse(res.begin(),res.end());
+// cout<<ans;
+//   int l=0;
+//   int r=0;
+//   int n=words.size();
+//   vector<string>ans;
+
+      return res;
+    }
+};
