361791740dd2f99b91e06bf0024e91f3e357d8f8
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-05-18 12:28:21 +0530

    Time: 87 ms (78.43%) | Memory: 129.9 MB (77.55%) - LeetSync

--- 2104-operations-on-tree/operations-on-tree.cpp
+++ 2104-operations-on-tree/operations-on-tree.cpp
@@ -1,123 +1,62 @@
 class LockingTree {
 public:
- unordered_map<int,vector<int>>adj;
-  unordered_map<int,int>lockmap;
-  unordered_map<int,vector<int>>Radj;
-    bool ancestorsCheck(int root)
-    {
-           queue<int>q;
-           q.push(root);
-           while(!q.empty())
-           {
-                   int n=q.size();
-                     for(int i=0;i<n;i++)
-                     {
-                          int node=q.front();
-                          q.pop();
-                             for(int a1:Radj[node])
-                             {
-                                      if(lockmap.find(a1)!=lockmap.end())return false;
-                                q.push(a1);
-                             }
-                     }
-           }
-           return true;
-    }
-     bool descendant (int root)
-    {
-           queue<int>q;
-           q.push(root);
-        
-           while(!q.empty())
-           {
-                 int n=q.size();
-                     for(int i=0;i<n;i++)
-                     {
-                          int node=q.front();
-                          q.pop();
-                             for(int a1:adj[node])
-                             {
-                                      if(lockmap.find(a1)!=lockmap.end())return true;
-                                q.push(a1);
-                             }
-                     }
-           }
-           return false;
-    }
-   void Unlockdescendant (int root)
-    {
-           queue<int>q;
-           q.push(root);
-        
-           while(!q.empty())
-           {
-                   int n=q.size();
-                     for(int i=0;i<n;i++)
-                     {
-                          int node=q.front();
-                          q.pop();
-                             for(int a1:adj[node])
-                             {
-                                      if(lockmap.find(a1)!=lockmap.end()){
-                                           lockmap.erase(a1);
-                                        }
-                                q.push(a1);
-                             }
-                     }
-           }
-        //    return false;
-    }
-    LockingTree(vector<int>& parent) {
-        for(int i=0;i<parent.size();i++)
-        {
-              if(parent[i]!=-1){
-                adj[parent[i]].push_back(i);
-                 Radj[i].push_back(parent[i]);
-              }
+    vector<int> parent;
+    vector<vector<int>> children;
+    vector<int> lockUser;  // lockUser[i] == 0 means unlocked
+
+    LockingTree(vector<int>& parent) : parent(parent), lockUser(parent.size(), 0) {
+        children.resize(parent.size());
+        for (int i = 0; i < parent.size(); ++i) {
+            if (parent[i] != -1)
+                children[parent[i]].push_back(i);
         }
     }
-    
-    bool lock(int num, int user) {
-          if(lockmap.find(num)==lockmap.end())
-          {
-                   lockmap[num]=user;
-                   return true;
-          }
-          return false;
 
+    bool lock(int num, int user) {
+        if (lockUser[num] != 0) return false;
+        lockUser[num] = user;
+        return true;
     }
-    
+
     bool unlock(int num, int user) {
-            
-              if(lockmap.find(num)!=lockmap.end())
-              {
-                      if(lockmap[num]==user)
-                      {
-                         lockmap.erase(num);
-                         return true;
-                      }else
-                      return false;
-              }
-              return false;
-    }
-    
-    bool upgrade(int num, int user) {
-        if(lockmap.find(num)!=lockmap.end())return false;
-        //  cout<<ancestorsCheck(num)<<" "<<descendant(num);
-         if(ancestorsCheck(num)&&descendant(num))
-         {
-              Unlockdescendant(num);
-              lockmap[num]=user;
-              return true;
-         }
+        if (lockUser[num] == user) {
+            lockUser[num] = 0;
+            return true;
+        }
         return false;
     }
-};
 
-/**
- * Your LockingTree object will be instantiated and called as such:
- * LockingTree* obj = new LockingTree(parent);
- * bool param_1 = obj->lock(num,user);
- * bool param_2 = obj->unlock(num,user);
- * bool param_3 = obj->upgrade(num,user);
- */
+    bool upgrade(int num, int user) {
+        if (lockUser[num] != 0) return false;
+        if (!areAllAncestorsUnlocked(num)) return false;
+
+        vector<int> lockedDescendants;
+        getLockedDescendants(num, lockedDescendants);
+
+        if (lockedDescendants.empty()) return false;
+
+        // Unlock all locked descendants
+        for (int node : lockedDescendants)
+            lockUser[node] = 0;
+
+        lockUser[num] = user;
+        return true;
+    }
+
+private:
+    bool areAllAncestorsUnlocked(int node) {
+        while (parent[node] != -1) {
+            node = parent[node];
+            if (lockUser[node] != 0) return false;
+        }
+        return true;
+    }
+
+    void getLockedDescendants(int node, vector<int>& lockedNodes) {
+        for (int child : children[node]) {
+            if (lockUser[child] != 0)
+                lockedNodes.push_back(child);
+            getLockedDescendants(child, lockedNodes);
+        }
+    }
+};
