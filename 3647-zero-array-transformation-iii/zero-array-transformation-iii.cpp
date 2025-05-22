0cdbca9ef90fbae090cc7fa081be873b2fd0d1a9
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-05-22 15:56:37 +0530

    Time: 1441 ms (5.06%) | Memory: 235.8 MB (12.53%) - LeetSync

---
+++ 3647-zero-array-transformation-iii/zero-array-transformation-iii.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
+        
+          priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+          for(int i=0;i<queries.size();i++)
+          {
+               int diff=queries[i][0]-queries[i][1];
+               pq.push({queries[i][0],queries[i][1]+1});
+          }
+
+          int ans=0;
+        //   int curr=-1;
+           priority_queue<int,vector<int>,greater<int>>curr;
+            priority_queue<int>box;
+
+           for(int i=0;i<nums.size();i++)
+           {
+                 while(!pq.empty()&&pq.top().first<=i)
+                 {
+                         box.push(pq.top().second);
+                         pq.pop();
+                 }
+                 int chala=0;
+                 while(!curr.empty()&&curr.top()<=i){
+                          chala++;        
+                 curr.pop();
+                 }
+                 int diff=nums[i]-curr.size();
+                 
+                 while(!box.empty()&&diff>0&&box.top()>i)
+                 {
+                    ans++;
+                            curr.push(box.top());
+                            box.pop();
+                            diff--;
+                 }
+                 cout<<nums[i]<<" "<<curr.size()<<endl;
+                 if(diff>0)return -1;
+                //  int siz=curr.size();
+                //  ans=max(ans,siz);
+
+
+
+           }
+           return queries.size()-ans;
+          
+    }
+};
