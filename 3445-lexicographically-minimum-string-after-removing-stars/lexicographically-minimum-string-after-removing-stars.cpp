4557f5ef41088aea28cc6a845d928da75077ea5a
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-06-07 17:30:54 +0530

    Time: 535 ms (11.36%) | Memory: 189.8 MB (5.84%) - LeetSync

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void solve( map<char,vector<int>>&m, unordered_map<int,int>&vm)
+    {
+            for(auto &a1:m)
+            {
+                //   cout<<a1.second[a1.second.size()-1]<<endl;
+                     vm[a1.second[a1.second.size()-1]]++;
+                     a1.second.pop_back();
+                     if(a1.second.size()<=0)
+                     m.erase(a1.first);
+                     return;
+            }
+    }
+    string clearStars(string s) {
+        map<char,vector<int>>m;
+        unordered_map<int,int>vm;
+        for(int i=0;i<s.length();i++)
+        {
+               if(s[i]!='*')
+               m[s[i]].push_back(i);
+               else{
+                      solve(m,vm);
+               }
+        }
+        string ans;
+        for(int i=0;i<s.length();i++)
+        {
+               if(s[i]=='*'||vm.find(i)!=vm.end())
+               {
+                    continue;
+               }
+               ans.push_back(s[i]);
+        }
+
+        return ans;
+    }
+};
