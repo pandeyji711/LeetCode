bead08204f5a74909ac1056e7ce4df223eec711b
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-06-17 23:46:57 +0530

    Time: 4 ms (10.99%) | Memory: 9.3 MB (14.37%) - LeetSync

---
+++ 3709-find-special-substring-of-length-k/find-special-substring-of-length-k.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+   bool ch(string s)
+   {
+     int k=s.length();
+         for(int i=0;i<k-1;i++)
+              {
+                   if(s[i]!=s[i+1])return false;
+              }
+              return true;
+   }
+    bool hasSpecialSubstring(string s, int k) {
+        if(s.length()==k)
+        {
+              for(int i=0;i<k-1;i++)
+              {
+                   if(s[i]!=s[i+1])return false;
+              }
+              return true;
+        }
+        for(int i=0;i<s.length()-k+1;i++)
+        {
+                    
+                  string str=s.substr(i,k);
+                  if(i==0)
+                  {
+                       if(str[str.length()-1]!=s[k+i]&&ch(str))return true;
+                  }
+                  else if(i>0&&i+k<s.length())
+                  {
+                              if(str[str.length()-1]!=s[k+i]&&ch(str)&&s[i-1]!=str[0])return true;
+                  }
+                  else if(i+k==s.length())
+                  {
+                              if(ch(str)&&s[i-1]!=str[0])return true;
+                  }
+
+
+        }
+        return false;
+
+    }
+};
