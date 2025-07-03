45c448b06ae43dcc835f766b6e08364e37d88aeb
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-07-03 10:44:09 +0530

    Time: 65 ms (18.03%) | Memory: 153.1 MB (18.18%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        priority_queue<pair<int,int>>pq;
-        //first window
-
-        for(int i=0;i<k;i++)
-        {
-              pq.push({nums[i],i});
-        }
+        priority_queue<pair<int,int>>pq; // first element is num form priority and second for index
+        // first window
+        for(int i=0;i<k;i++)    
+        {   pq.push({nums[i],i});
+         }
         vector<int>ans;
-        ans.push_back(pq.top().first);
-        int l=0;
-        int r=k;
+        ans.push_back(pq.top().first); //pq.top() gives max element
+        int l=0,r=k;
         while(r<nums.size())
-        {
-              pq.push({nums[r],r});
-              l++;
-              while(!pq.empty()&&pq.top().second<l)
-              pq.pop();
-              ans.push_back(pq.top().first);
-              r++;
+        { pq.push({nums[r],r});  //push the element of new window
+             l++; // srink the window
+              while(!pq.empty()&&pq.top().second<l) 
+              pq.pop();   // remove expire element not belong to thst window
+              ans.push_back(pq.top().first); //add the max element form that window
+              r++;   // expand the windo
+              }
+        return ans; 
         }
-        return ans;
-    }
 };
