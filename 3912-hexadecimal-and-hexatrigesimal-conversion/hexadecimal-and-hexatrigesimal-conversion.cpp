51f861e35f6b1afd386af4209de6b0ed134a6b91
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-07-06 10:35:42 +0530

    Time: 12 ms (5.14%) | Memory: 8.4 MB (16.67%) - LeetSync

---
+++ 3912-hexadecimal-and-hexatrigesimal-conversion/hexadecimal-and-hexatrigesimal-conversion.cpp
@@ -0,0 +1,105 @@
+class Solution {
+public:
+   string hex(int n)
+   {
+         n=n*n;
+         string s;
+             if(n<=9)
+              {
+                   s.push_back(n+'0');
+                   return s;
+                   
+              }else if(n<16) {
+                       n=n-10;
+                       char c='A'+n;
+                       s.push_back(c);
+                       return s;
+              }
+         while(n>1)
+         {
+             
+               int re=n%16;
+                if(re<=9)
+              {
+                 cout<<re<<" ";
+                   s.push_back(re+'0');
+                   
+              }else {
+                       re=re-10;
+                       char c='A'+re;
+                       s.push_back(c);
+              }
+               n=n/16;
+                if(n<=9)
+              {
+                   s.push_back(n+'0');
+                   return s;
+                   
+              }else if(n<16) {
+                       n=n-10;
+                       char c='A'+n;
+                       s.push_back(c);
+                       return s;
+              }
+
+
+         }
+         return s;
+   }
+    string thex(int n)
+   {
+         n=n*n*n;
+         string s;
+             if(n<=9)
+              {
+                   s.push_back(n+'0');
+                   return s;
+                   
+              }else if(n<36) {
+                       n=n-10;
+                       char c='A'+n;
+                       s.push_back(c);
+                       return s;
+              }
+         while(n>1)
+         {
+             
+               int re=n%36;
+                if(re<=9)
+              {
+                 cout<<re<<" ";
+                   s.push_back(re+'0');
+                   
+              }else {
+                       re=re-10;
+                       char c='A'+re;
+                       s.push_back(c);
+              }
+               n=n/36;
+                if(n<=9)
+              {
+                   s.push_back(n+'0');
+                   return s;
+                   
+              }else if(n<36) {
+                       n=n-10;
+                       char c='A'+n;
+                       s.push_back(c);
+                       return s;
+              }
+
+
+         }
+         return s;
+   }
+    string concatHex36(int n) {
+         string s1;
+         string s2;
+          s1=hex(n);
+          s2=thex(n);
+          reverse(s1.begin(),s1.end());
+          reverse(s2.begin(),s2.end());
+
+          return s1+s2;
+    }
+};
