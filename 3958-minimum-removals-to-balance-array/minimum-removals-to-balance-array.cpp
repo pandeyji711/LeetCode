f2e8da438f3a9983e9dcae68f9d2a123af44536b
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-08-03 18:57:29 +0530

    Time: 78 ms (11.11%) | Memory: 124.1 MB (0.00%) - LeetSync

---
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        queue<pair<int ,long long >>q;
+        int ans=INT_MAX;
+        for(int i=0;i<nums.size();i++)
+        {
+                      while(!q.empty()&&q.front().second<nums[i])
+                      {
+                                int tem=q.front().first+nums.size()-i;
+                                ans=min(ans,tem);
+                                q.pop();
+                      }
+                      q.push({i,1LL*k*nums[i]});
+        }
+        while(!q.empty())
+        {
+                int tem=q.front().first;
+                                ans=min(ans,tem);
+                                q.pop();
+        }
+        return ans==INT_MAX?0:ans;
+    }
+};
