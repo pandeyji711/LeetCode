60c15d33bbe67645f38888b0ec5339f61528a8c0
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-08-07 20:14:40 +0530

    Time: 11 ms (67.88%) | Memory: 13.8 MB (93.31%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,30 +1,65 @@
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
-        
-        int ans=0;
-        int diff=INT_MAX;
-        for(int i=0;i<nums.size();i++)
-        {
-             for(int j=i+1;j<nums.size();j++)
-             {
-                  for(int k=j+1;k<nums.size();k++)
-                  {
-                            int tem=nums[i]+nums[j]+nums[k];
-                            int di=abs(target-tem);
-                            if(di==0)
-                            {
-                                // cout<<tem<<" "<<nums[i]<<" "<<nums[j]<<" "<<nums[k]<<endl;
-                                return target;
-                            }else if(di<diff)
-                            {
-                                  ans=tem;
-                                //   cout<<tem<<" ";
-                                  diff=di;
+  int solve(vector<int>& nums,int mid)
+  {
+      int ans=0;
+       int diff=INT_MAX;
+            for(int i=0;i<nums.size();i++)
+            {
+                   int req=mid-nums[i];
+                   int l=i+1;
+                   int r=nums.size()-1;
+                   while(l<r)
+                   {
+                         if(nums[l]+nums[r]==req)
+                         {
+                            // cout<<"k";
+                            return mid;
                             }
-                  }
-             }
-        }
-        return ans;
+                          if(diff>abs(mid-(nums[l]+nums[r]+nums[i])))
+                                {
+                                    // cout<<i<<" "<<l<<" "<<r<<endl;
+                                //    cout<< nums[l]<<" "<<nums[r]<<" "<<nums[i]<<endl;
+                                      ans=nums[l]+nums[r]+nums[i];
+                                    //   cout<<ans<<" ";
+                                      diff=abs(mid-(nums[l]+nums[r]+nums[i]));
+                                }
+                         
+
+                         else if(nums[l]+nums[r]<req)
+                         {  
+                               
+                              l++;
+                         }else{
+                            r--;
+                         }
+
+                   }
+            }
+            // cout<<ans<<" ";
+            return ans;
+
+  }
+    int threeSumClosest(vector<int>& nums, int target) {
+        // int l=0;
+        // int r=4000;
+        // int ans=0;
+        sort(nums.begin(),nums.end());
+        // // cout<<solve(nums,2);
+        // while(l<=r)
+        // {
+        //       int mid=l+(r-l)/2;
+        //       int n1=target+mid;
+        //       int n2=target-mid;
+        //       if(solve(nums,n1)||solve(nums,n2))
+        //       {
+        //         cout<<"k";
+        //            ans=n2;
+        //            r=mid-1;
+        //       }else
+        //        l=mid+1;
+
+        // }
+        return solve(nums,target);
     }
 };
