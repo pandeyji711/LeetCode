5fa63512fcb81ba17f02ae471a8ce96c9682edc2
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-08-10 18:30:32 +0530

    Time: 1606 ms (5.01%) | Memory: 9 MB (62.83%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+ bool po2(int n)
+ {
+         if(n<=0)return true;
+         while(n>1)
+         {
+              if(n%2!=0)return false;
+              n=n/2;
+         }
+         return true;
+ }
+    bool reorderedPowerOf2(int n) {
+        string str=to_string(n);
+        sort(str.begin(),str.end());
+        do{
+            if(str[0]=='0')continue;
+               int n1=stoi(str);
+
+
+               cout<<n1<<" ";
+               if(po2(n1))return true;
+        } while(next_permutation(str.begin(),str.end()));
+
+           return false;
+
+    }
+};
