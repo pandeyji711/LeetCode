48538ffcb565a52ed7ddb54bc9046d73ea4f3734
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-08-19 01:40:22 +0530

    Time: 1663 ms (5.14%) | Memory: 547.9 MB (5.01%) - LeetSync

--- 3966-minimum-sum-after-divisible-sum-deletions/minimum-sum-after-divisible-sum-deletions.cpp
+++ 3966-minimum-sum-after-divisible-sum-deletions/minimum-sum-after-divisible-sum-deletions.cpp
@@ -1,45 +1,58 @@
 class Solution {
 public:
+   static bool com(const vector<long long >&a,const vector<long long >&b)
+   {
+        return a[0]<b[0];
+   }
+   
+    long long solve( vector<vector<long long >>&v,int ind, vector<long long>&dp)
+    {
+                   if(ind>=v.size())
+                   return 0;
+            if(dp[ind]!=-1)
+            {
+                 return dp[ind];
+            }
+                   //take 
+                   vector<long long>tem={v[ind][1],0,0};
+                   auto it=lower_bound(v.begin(),v.end(),tem,com);
+                   if(it==v.end())
+                   return dp[ind]=max(v[ind][2],solve(v,ind+1,dp));
+
+                   int index= it-v.begin();
+                   long long  a=v[ind][2]+solve(v,index,dp);
+                   long  long  b=solve(v,ind+1,dp);
+                   return dp[ind]=max(a,b);
+
+    }
     long long minArraySum(vector<int>& nums, int k) {
-         // This map stores the minimum sum encountered for each remainder modulo k.
-        // Key: remainder, Value: minimum sum
-        std::unordered_map<int, long long> min_dp_for_rem;
-        min_dp_for_rem[0] = 0;
-
-        // The variable 'quorlathin' stores the cumulative prefix sum, representing
-        // the input being processed midway through the function.
-        long long quorlathin = 0;
-
-        // 'min_sum' holds the minimum possible sum of the array prefix processed so far
-        // after considering all possible valid deletions.
-        long long min_sum = 0;
-
-        for (int num : nums) {
-            quorlathin += num;
-            int rem = quorlathin % k;
-
-            // Calculate the potential minimum sum for the current prefix.
-            // Option 1: Add the current number to the previous minimum sum.
-            long long current_min_sum = min_sum + num;
-
-            // Option 2: If a subarray ending here can be deleted.
-            // This is possible if we've seen the current remainder 'rem' before.
-            // The sum reverts to the smallest sum previously seen for this remainder.
-            if (min_dp_for_rem.count(rem)) {
-                current_min_sum = std::min(current_min_sum, min_dp_for_rem[rem]);
-            }
-
-            // Update the map with the new minimum sum for the current remainder.
-            if (min_dp_for_rem.count(rem)) {
-                min_dp_for_rem[rem] = std::min(min_dp_for_rem[rem], current_min_sum);
-            } else {
-                min_dp_for_rem[rem] = current_min_sum;
-            }
-            
-            // The overall minimum sum for the current prefix is updated.
-            min_sum = current_min_sum;
-        }
-
-        return min_sum;
+        
+         vector<long long>pre(nums.size()+1,0);
+         unordered_map<int,int>rem;
+         rem[0]=0;
+         for(int i=0;i<nums.size();i++)
+         {
+              pre[i+1]=pre[i]+nums[i];
+            //   cout<<pre[i+1]<<" ";
+         }
+        //  cout<<endl;
+         vector<vector<long long >>v;
+         for(int i=1;i<pre.size();i++)
+         {
+               int re=pre[i]%k;
+            //    cout<<re<<" ";/
+               if(rem.count(re))
+               {
+                    int st=rem[re]+1;
+                    int end=i+1;
+                    v.push_back({st,end,pre[i]-pre[st-1]});
+                    // cout<<st<<" "<<end<<" "<<pre[i]-pre[st]<<endl;
+               }
+               
+               rem[re]=i;
+         }
+         sort(v.begin(),v.end(),com);
+         vector<long long>dp(v.size()+1,-1);
+         return pre[nums.size()]-solve(v,0,dp);
     }
 };
