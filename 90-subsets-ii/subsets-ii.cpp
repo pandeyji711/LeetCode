b8eaa4fef3c75d2c10d177a444601d14ccbc5d25
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-08-21 14:56:50 +0530

    Time: 10 ms (9.08%) | Memory: 18.1 MB (6.59%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+  vector<vector<int>>ans;
+  map<vector<int>,int>m;
+   void solve(vector<int>& nums,int ind, vector<int>tem)
+   {
+       if(ind>=nums.size())
+       {
+          sort(tem.begin(),tem.end());
+            if(!m.count(tem)){
+            ans.push_back(tem);
+               m[tem]=1;
+            }
+            
+            return;
+       }
+       tem.push_back(nums[ind]);
+       solve(nums,ind+1,tem);
+       tem.pop_back();
+       solve(nums,ind+1,tem);
+
+   }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int>tem;
+        solve(nums,0,tem);
+        return ans;
+    }
+};
