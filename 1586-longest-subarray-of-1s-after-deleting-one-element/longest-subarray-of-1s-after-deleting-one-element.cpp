80a6a88d951aaa35e8edd8d6fc1f0187cc19ec56
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-08-24 13:55:14 +0530

    Time: 14 ms (5.66%) | Memory: 60.2 MB (23.29%) - LeetSync

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        vector<pair<int,int>>vp;
+        int dub=nums[0];
+        int cnt=1;
+        for(int i=1;i<nums.size();i++)
+        { 
+                if(dub!=nums[i])
+                {
+                     cout<<cnt<<" "<<dub<<endl;
+                      vp.push_back({cnt,dub});
+                      cnt=0;
+                     
+                      dub=nums[i];
+                }
+                cnt++;
+        }
+         vp.push_back({cnt,dub});
+         cout<<cnt<<" "<<dub<<endl;
+         int ans=0;
+         bool de=false;
+         for(int i=0;i<vp.size();i++)
+         {
+              if(vp[i].second==1&&ans<vp[i].first){
+                 de=false;
+                 ans=vp[i].first;
+
+              }
+                  if(i+2<vp.size())
+                  {
+                       if(vp[i].second==1&&vp[i+1].second==0&&vp[i+2].second==1)
+                       {
+                              if(vp[i+1].first==1)
+                              {
+                                   if(ans<=vp[i].first+vp[i+2].first)
+                                     {
+                                          ans=vp[i].first+vp[i+2].first;
+                                          de=true;
+                                     }
+                              }
+                       }
+                  }
+         }
+         if(ans==0)
+         return ans;
+           if(vp.size()==1)
+         return ans-1;
+         
+         return ans;
+       
+
+
+    }
+};
