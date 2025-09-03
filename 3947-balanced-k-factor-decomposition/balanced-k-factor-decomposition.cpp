550d60af3df0509a3066ec5dfa039aeca0f59a77
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-09-03 18:56:07 +0530

    Time: 31 ms (60.33%) | Memory: 10 MB (55.39%) - LeetSync

---
+++ 3947-balanced-k-factor-decomposition/balanced-k-factor-decomposition.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+  vector<int>ans;
+  int diff=INT_MAX;
+  void solve(vector<int>&div,int k,int n,vector<int>&curr)
+  {
+          if(k==1)
+          {
+               curr.push_back(n);
+               int minn=*min_element(curr.begin(),curr.end());
+               int maxx=*max_element(curr.begin(),curr.end());
+               if(maxx-minn<diff)
+               {
+                  diff=maxx-minn;
+                  ans=curr;
+               }
+                if(curr.size()>0)
+               curr.pop_back();
+               return ;
+
+          }
+          for(int d:div)
+          {  
+                   if(d>n)break;
+               if(n%d==0)
+               {
+                  curr.push_back(d);
+                    solve(div,k-1,n/d,curr);
+                    curr.pop_back();
+               }
+          }
+
+                 
+  }
+
+    vector<int> minDifference(int n, int k) {
+         vector<int>div;
+         div.push_back(1);
+      for(int i=2;i<=n;i++)
+      {
+          if(n%i==0){
+          div.push_back(i);
+               
+          }
+
+      }
+      sort(div.begin(),div.end());
+      vector<int>curr;
+    //    cout<<div.size()<< " ";
+    solve(div,k,n,curr);
+        return ans;
+    }
+};
