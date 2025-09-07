61440737b981333a35dc2b3912b352947767f1f8
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-09-08 02:16:52 +0530

    Time: 75 ms (7.14%) | Memory: 133 MB (7.14%) - LeetSync

--- 4000-count-bowl-subarrays/count-bowl-subarrays.cpp
+++ 4000-count-bowl-subarrays/count-bowl-subarrays.cpp
@@ -2,21 +2,14 @@ class Solution {
 public:
     long long bowlSubarrays(vector<int>& nums) {
         long long  ans=0;
-        queue<int>q;
+      
         stack<pair<int,int>>st;
-        int cnt=0;
-        int currcnt=0;
+      
         for(int i=0;i<nums.size();i++)
         {
-              bool ga=false;
-                //   while(!q.empty()&&q.front()==i)
-                //   {
-                //       cnt++;
-                //       q.pop();
-                //   }
                  while(!st.empty()&&st.top().first<nums[i])
                   {
-                        //  cout<<st.top().first<<" "<<st.top().second<<endl;
+                       
                             if(i-st.top().second+1>=3)
                               ans++;
                               st.pop();
@@ -27,20 +20,12 @@ public:
                   st.push({nums[i],i});
 
         }
-        // cout<<ans<<endl;
-         queue<int>q1;
+        
         stack<pair<int,int>>st1;
-        int cnt1=0;
-        int currcnt1=0;
         int n=nums.size();
        for(int i=n-1;i>=0;i--)
         {
-              bool ga=false;
-                //   while(!q1.empty()&&q1.front()==i)
-                //   {
-                //       cnt1++;
-                //       q1.pop();
-                //   }
+            
                  while(!st1.empty()&&st1.top().first<nums[i])
                   {
                           if(abs(st1.top().second-i+1>=3))
