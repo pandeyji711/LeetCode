877882b2b5bdeb6bd2cbb6263233a3fe65c2479f
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-09-14 15:38:57 +0530

    Time: 83 ms (100.00%) | Memory: 134.9 MB (100.00%) - LeetSync

---
+++ 3953-minimum-discards-to-balance-inventory/minimum-discards-to-balance-inventory.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minArrivalsToDiscard(vector<int>& arrivals, int w, int m) {
+        unordered_map<int,int>mp;
+        
+        int l=0;
+        int r=0;
+        int ans=0;
+        int rand=INT_MIN;
+        vector<bool>keep(arrivals.size(),true);
+        while(r<arrivals.size())
+        {
+               mp[arrivals[r]]++;
+               if(r-l+1>w)
+               {
+                  if(keep[l])
+                  mp[arrivals[l]]--;
+                  l++;
+               }
+               if(mp[arrivals[r]]>m)
+               {
+                  keep[r]=false;
+                    mp[arrivals[r]]--;
+                    // mp[rand]++;
+                    // rand++;
+                    ans++;
+                    
+                    
+               }
+               r++;
+        }
+        return ans;
+    }
+};
+// [3,3,3,3,3,3,3]
