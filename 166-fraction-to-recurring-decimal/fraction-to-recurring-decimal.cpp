8ddfe784193ddc47abacce45ac3a74f372ba8251
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-09-25 01:07:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (63.93%) - LeetSync

---
+++ 166-fraction-to-recurring-decimal/fraction-to-recurring-decimal.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string fractionToDecimal(int numerator, int denominator) {
+            if(numerator==0)return "0";
+
+               string ans="";
+            if((numerator<0)^(denominator<0))
+               ans+="-";
+               long long de=llabs((long long )denominator);
+               long long nu=llabs((long long )numerator);
+           ans+=to_string(nu/de);
+            if(nu%de==0)
+            return ans;
+            ans+=".";
+           unordered_map<long long,int>m;
+          long long  rem=nu%de;
+             while(rem!=0)
+             {
+                     if(m.count(rem))
+                     {
+                            ans.insert(m[rem],"(");
+                            ans+=")";
+                            break;
+                     }
+                     m[rem]=ans.size();
+                     rem*=10;
+                     ans+=to_string(rem/de);
+                     rem%=de;
+             }
+             return ans;
+    }
+};
