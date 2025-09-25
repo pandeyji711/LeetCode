0f263b3b2624d82b4117868a2e0389c0a8e53cb9
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-09-25 15:19:05 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (22.22%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
- vector<vector<int>>dp;
-  int solve(vector<vector<int>>& triangle, int r, int i)
-  {
-                   if(r==triangle.size()-1)
-                   {
-                       return dp[r][i]=triangle[r][i];
-                   }
-                   if(dp[r][i]!=-1)return dp[r][i];
-                   int x=0;
-                   int y=0;
-                   x=triangle[r][i]+solve(triangle,r+1,i);
-                    y=triangle[r][i]+solve(triangle,r+1,i+1);
-                   return dp[r][i]=min(x,y);
-  }
+ int solve(vector<vector<int>>& triangle,int r,int c, vector<vector<int>>&dp)
+ {
+            if(r>=triangle.size())
+            return 0;
+
+           if(dp[r][c]!=-1)
+           return dp[r][c];
+            int b=0;
+          if(r+1<triangle.size()&&c+1<triangle[r+1].size())
+           b=solve(triangle,r+1,c+1,dp);
+
+           return  dp[r][c]= min(b,solve(triangle,r+1,c,dp))+triangle[r][c];
+ }
     int minimumTotal(vector<vector<int>>& triangle) {
-       int n=triangle.size();
-       int m=triangle[triangle.size()-1].size();
-        dp.resize(n+1,vector<int>(m+1,-1));
-        return solve(triangle,0,0);
+        vector<vector<int>>dp(triangle.size()+1,vector<int>(triangle.size()+1,0));
+         for(int i=triangle.size()-1;i>=0;i--)
+         {
+               for(int j=0;j<triangle[i].size();j++)
+               {
+                     dp[i][j] =triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+               }
+         }
+        return dp[0][0];
     }
 };
