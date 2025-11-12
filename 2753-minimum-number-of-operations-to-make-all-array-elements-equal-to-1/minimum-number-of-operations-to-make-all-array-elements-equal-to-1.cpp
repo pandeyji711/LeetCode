4352f88395edc14b3e5533784b86233610eb8295
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2025-11-12 22:47:16 +0530

    Time: 3 ms (15.97%) | Memory: 32.7 MB (5.56%) - LeetSync

---
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int solve(vector<int> nums,int ind)
+    {
+            int i=ind;
+            int cnt=0;
+            while(i>=0)
+            {
+                     if(gcd(nums[i],nums[i+1])<nums[i])
+                     {
+                        
+                         if(gcd(nums[i],nums[i+1])==1)return cnt+1;
+                           nums[i]=gcd(nums[i],nums[i+1]);
+                        //    cout<<nums[i]<<" "<<i<<endl;
+                          
+                           cnt++;
+                     }
+                        i--;
+                     
+                     
+            }
+            return -1;
+    }
+    int minOperations(vector<int>& nums) {
+        int ans=INT_MAX; 
+        int n=nums.size();
+         int ones = count(nums.begin(), nums.end(), 1);
+
+        // Case 1: if array already contains ones
+        if (ones > 0) 
+            return n - ones;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+             int tem=solve(nums,i);
+                 if(tem!=-1)
+                 {
+                     ans=min(ans,tem);
+                 }
+        }
+        // cout<<ans;
+        return ans==INT_MAX?-1:ans+nums.size()-1;
+    
+    }
+};
