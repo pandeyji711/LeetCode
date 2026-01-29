9d2d618ab0b9f634b90c55064dffb8b79795e761
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2026-01-29 22:10:21 +0530

    Time: 71 ms (5.00%) | Memory: 15.1 MB (79.43%) - LeetSync

---
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+ int dp[1001][1001];
+  int solve(vector<int>&v1, vector<int>&v2,int ind1,int ind2)
+  {
+          if(ind1>=v1.size()||ind2>=v2.size())return 0;
+          if(dp[ind1][ind2]!=-1)
+          return dp[ind1][ind2];
+          int a=0;
+          int b=0;
+          if(v1[ind1]==v2[ind2])
+          {
+              a=v1[ind1]+solve(v1,v2,ind1+1,ind2+1);
+          }
+          else{
+                b=max({b,solve(v1,v2,ind1+1,ind2),solve(v1,v2,ind1,ind2+1)});
+          }
+          return dp[ind1][ind2]=max(a,b);
+  }
+    int minimumDeleteSum(string s1, string s2) {
+        memset(dp,-1,sizeof(dp));
+        vector<int>v1,v2;
+        int maxx=0;
+        for(int i=0;i<s1.length();i++)
+        {
+             int c1=s1[i];
+             maxx+=c1;
+             v1.push_back(c1);
+        }
+         for(int i=0;i<s2.length();i++)
+        {
+             int c1=s2[i];
+             maxx+=c1;
+             v2.push_back(c1);
+        }
+        return maxx-2*solve(v1,v2,0,0);
+    }
+};
