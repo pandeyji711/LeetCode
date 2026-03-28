25e953924465f9a217aff51e5eadf84268c10495
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2026-03-29 00:53:41 +0530

    Time: 0 ms (100.00%) | Memory: 32.8 MB (17.92%) - LeetSync

---
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+     void left(vector<vector<int>>& mat,int ind)
+     {
+                reverse(mat[ind].begin(),mat[ind].begin()+1);
+                reverse(mat[ind].begin()+1,mat[ind].end());
+                reverse(mat[ind].begin(),mat[ind].end());
+
+     }
+         void right(vector<vector<int>>& mat,int ind)
+     {
+         reverse(mat[ind].begin(),mat[ind].end());
+                reverse(mat[ind].begin(),mat[ind].begin()+1);
+                reverse(mat[ind].begin()+1,mat[ind].end());
+                
+
+     }
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+       vector<vector<int>>v=mat;
+       while(k--){
+       for(int i=0;i<mat.size();i++)
+       {
+             if(i%2==0)
+             {
+                left(mat,i);
+             }else{
+                 right(mat,i);
+             }
+       }
+
+
+
+       }
+       return mat==v;
+
+    }
+};
