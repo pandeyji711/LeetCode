294ced230051bd427fc25d8f7b202d7a84daea28
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2026-05-17 12:04:08 +0530

    Time: 0 ms (100.00%) | Memory: 35.4 MB (71.26%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-//  bool solve(vector<int>& arr,int ind)
-//  {
-        
-//             queue<int>q;
-            
 
-//  }
     bool canReach(vector<int>& arr, int start) {
         vector<bool>tem(arr.size(),false);
         queue<int>q;
         tem[start]=true;
         q.push(start);
+        if(arr[start]==0)return true;
         while(!q.empty())
         {
                int n=q.size();
@@ -24,22 +19,24 @@ public:
                     //   cout<<front<<" "<<back<<endl;
                       if(front!=-1&&tem[ind+arr[ind]]==false)
                       {
+                         if(arr[ind+arr[ind]]==0)return true;
                            q.push(ind+arr[ind]);
+                          
                            tem[ind+arr[ind]]=true;
                       }
                        if(back!=-1&&tem[ind-arr[ind]]==false)
-                      {
+                      {  if(arr[ind-arr[ind]]==0)return true;
                            q.push(ind-arr[ind]);
                            tem[ind-arr[ind]]=true;
                       }
                }
         }
-        for(int i=0;i<arr.size();i++)
-        {
-              if(arr[i]==0&&tem[i])
-              return true;
-            //   cout<<tem[i]<<" ";
-        }
+        // for(int i=0;i<arr.size();i++)
+        // {
+        //       if(arr[i]==0&&tem[i])
+        //       return true;
+        //     //   cout<<tem[i]<<" ";
+        // }
         return false;
     }
 };
