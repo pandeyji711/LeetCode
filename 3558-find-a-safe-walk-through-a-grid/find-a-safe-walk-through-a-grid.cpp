93b089c67457a6d34a48e1affd212decf6bdb8d6
Anurag Pandey <154527778+pandeyji711@users.noreply.github.com>
2026-07-17 21:01:17 +0530

    Time: 2650 ms (5.02%) | Memory: 63 MB (5.36%) - LeetSync

---
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+       vector<vector<int>>vis(grid.size(),vector<int>(grid[0].size(),0));
+       queue<vector<int>>q;
+       q.push({0,0,health-grid[0][0]});
+       vis[0][0]=health-grid[0][0];
+       int cnt=0;
+       while(!q.empty())
+       {
+            int n=q.size();
+              for(int i=0;i<n;i++)
+              {
+                 int x=q.front()[0];
+                 int y=q.front()[1];
+                  int h=q.front()[2];
+                  q.pop();
+                  if(x-1>=0)
+                  {
+                        if(vis[x-1][y]<h-(grid[x-1][y]==1?1:0))
+                        {
+                             vis[x-1][y]=h-(grid[x-1][y]==1?1:0);
+                             q.push({x-1,y,vis[x-1][y]});
+                              cout<<vis[x-1][y]<<"U"<<x-1<<" "<<y<<endl;
+                        }
+                  }
+                   if(x+1<grid.size())
+                  {
+                        if(vis[x+1][y]<h-(grid[x+1][y]==1?1:0))
+                        {
+                             vis[x+1][y]=h-(grid[x+1][y]==1?1:0);
+                             q.push({x+1,y,vis[x+1][y]});
+                             cout<<vis[x+1][y]<<"D"<<x+1<<" "<<y<<endl;
+                        }
+                  }
+                   if(y-1>=0)
+                  {
+                        if(vis[x][y-1]<h-(grid[x][y-1]==1?1:0))
+                        {
+                             vis[x][y-1]=h-(grid[x][y-1]==1?1:0);
+                             q.push({x,y-1,vis[x][y-1]});
+                             cout<<vis[x][y-1]<<"L"<<x<<" "<<y-1<<endl;
+                             
+                        }
+                  }
+                   if(y+1<grid[0].size())
+                  {
+                        if(vis[x][y+1]<h-(grid[x][y+1]==1?1:0))
+                        {
+                             vis[x][y+1]=h-(grid[x][y+1]==1?1:0);
+                             q.push({x,y+1,vis[x][y+1]});
+                              cout<<vis[x][y+1]<<"R"<<x<<" "<<y+1<<endl;
+                        }
+                  }
+              }
+       }
+       cout<<vis[grid.size()-1][grid[0].size()-1];
+       return vis[grid.size()-1][grid[0].size()-1]>0?true:false;
+    }
+};
